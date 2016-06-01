21e77bca00be1e7d097573ae87849ce7f913f64a
okabe-masamune <m.masadayo@gmail.com>
2016-06-01 13:50:00 +0900

    オブジェクト指向１　課題1～４

---
+++ Machine.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package object.sample;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author masa1
+ */
+public class Machine {
+    public Machine(){}
+ public String name; 
+ public int money;
+ public void setHen(){
+     name = "電車";
+     money = 100000000;
+ }
+}
+
+     
+
---
+++ spaMachine.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package object.sample;
+
+/**
+ *
+ * @author masa1
+ */
+public class spaMachine extends Machine {
+     public void setC(){
+         name ="";
+         money =0;  
+}
+}
